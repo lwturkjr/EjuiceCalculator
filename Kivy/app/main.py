44b6c1001537f37f7c83965e9d816f4a43c09d27
lwturkjr <lwturkjr@SgtSegFault.home>
2021-07-30 03:58:30 -0400

    IDK been a while, going to try to do things this way, instead of how i was orginally planning, becuase, idk this way seems easier. I will probably do seperate screen kvs and scripts later. I just want to get some progress done, and get something to work, and while this isn't the most optimized way it should work.

--- Kivy/app/main.py
+++ Kivy/app/main.py
@@ -1,30 +1,12 @@
 #! /usr/bin/env python3
-import os
-import sys
-import json
-import pathlib
-
-
-from kivy.app import App
-from kivy.lang import Builder
+from kivy.app import App 
+from kivy.uix.widget import Widget 
+from kivy.lang import Builder 
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import ObjectProperty
-from kivy.uix.popup import Popup
-from kivy.uix.label import Label
-from kivy.config import Config
 
-Config.set("kivy","window_icon","icon.ico")
-
-path = os.path.join(sys.path[0])
-
+#Define screens
 class MainMenu(Screen):
-    mainmenu = ObjectProperty(None)
-    settings = ObjectProperty(None)
-    calculator = ObjectProperty(None)
-    savedrecipes = ObjectProperty(None)
-    current = ""
-
-
+    pass
 
 class CalcWindow(Screen):
     pass
@@ -32,24 +14,20 @@ class CalcWindow(Screen):
 class SavedRecipes(Screen):
     pass
 
-class Settings(Screen):
+class AvilableFlavors(Screen):
+    pass
+
+class SettingsScreen(Screen):
     pass
 
 class WindowManager(ScreenManager):
     pass
 
 kv = Builder.load_file("my.kv")
-wm = WindowManager()
-saved_recipes_dir = f"{path}/saved_recipes/"
-
-screens = [MainMenu(name="main"), CalcWindow(name="calc"),SavedRecipes(name="saved"), Settings(name="settings")]
-for screen in screens:
-    wm.add_widget(screen)
-
 
 class EJuiceWorkshop(App):
     def build(self):
-        return wm
+        return kv
 
 if __name__ == "__main__":
     EJuiceWorkshop().run()
